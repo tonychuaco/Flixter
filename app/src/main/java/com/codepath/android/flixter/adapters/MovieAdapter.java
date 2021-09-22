0e644bfeb3e3ca9c9dbbe9c543601eb4bfd8f5bd
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2021-09-22 13:14:39 -0400

    Part 2: Add DetailActivity. Allow YouTubePlayerView videos.

--- app/src/main/java/com/codepath/android/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/android/flixter/adapters/MovieAdapter.java
@@ -1,22 +1,27 @@
 package com.codepath.android.flixter.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.android.flixter.DetailActivity;
 import com.codepath.android.flixter.R;
 import com.codepath.android.flixter.models.Movie;
 
 import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,6 +58,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
   public class ViewHolder extends RecyclerView.ViewHolder {
 
+    RelativeLayout container;
     TextView tvTitle;
     TextView tvOverview;
     ImageView ivPoster;
@@ -62,6 +68,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
       tvTitle = itemView.findViewById(R.id.tvTitle);
       tvOverview = itemView.findViewById(R.id.tvOverview);
       ivPoster = itemView.findViewById(R.id.ivPoster);
+      container = itemView.findViewById(R.id.container);
     }
 
     public void bind(Movie movie) {
@@ -75,6 +82,15 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
       }
 
       Glide.with(context).load(imageUrl).into(ivPoster);
+
+      container.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent i = new Intent(context, DetailActivity.class);
+          i.putExtra("movie", Parcels.wrap(movie));
+          context.startActivity(i);
+        }
+      });
     }
   }
 }
